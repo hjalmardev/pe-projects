f0887c1bca7472cfab2e4520ac1a86748c08ea93
Hjalmar <hello@hjalmar.dev>
2022-10-01 23:46:15 -0400

    Create a website for a pet adoption service

---
+++ monsters/index.php
@@ -0,0 +1,29 @@
+<?php
+
+	$cody = [
+		"id" => 1856,
+		"name" => "Codey",
+		"favoriteFood" => "Mountain Dew Code Red",
+		"age" => 7,
+		"adopted" => true,
+	];
+
+	$lima = [
+		"id" => 1256,
+		"name" => "Limabean",
+		"favoriteFood" => "Lima beans",
+		"age" => 4,
+		"adopted" => false,
+	];
+
+	$reads = [
+		"id" => 764,
+		"name" => "Miss Reads-a-Lot",
+		"favoriteFood" => "Madeline cookies and tea",
+		"age" => 9,
+		"adopted" => false,
+	];
+
+	$monsters = [$cody, $lima, $reads];
+
+	foreach ()
