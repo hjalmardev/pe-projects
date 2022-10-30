8b65583121154a1f8ef8f9164aba39dedaf6402b
Hjalmar Rivera <hello@hjalmar.dev>
2022-10-29 20:12:19 -0400

    Add form validation and data structure

--- projects/php/create.php
+++ projects/php/create.php
@@ -1,8 +1,66 @@
 <?php
+
+	$title = "";
+	$description  = "";
+	$id = 0;
+
+	$hasTitle = false;
+	$hasDescription = false;
+
+	$titleError = false;
+	$descriptionError = false;
+
+	// $todo: check if ID already exists before adding number
 	if ( isset($_POST['add']) ) {
-		echo "added project";
+		
+		if ( isset($_POST['title']) ) {
+			$title = $_POST['title'];
+			
+			if ( strlen($title) > 0 ) {
+				$hasTitle = true;
+			} else {
+				$titleError = "Please add project title.";
+			}
+		}
+	}
+
+	if ( isset($_POST['description']) ) {
+		
+		if ( isset($_POST['description']) ) {
+			$description = $_POST['description'];
+			
+			if ( strlen($description) > 0 ) {
+				$hasDescription = true;
+			} else {
+				$descriptionError = "Please add project description.";
+			}
+		}
+	}
+
+	if ( isset($_POST['add']) ) {
+		
+		if ( isset($_POST['id']) ) {
+			$id = $_POST['id'];
+			
+			if ( intval($id) > 0 ) {
+				$hasid = true;
+			} else {
+				$titleid = "Please add ID number.";
+			}
+		}
+	}
+
+	if ($hasTitle && $hasDescription) {
+		$project = [
+			$title => $_POST['title'],
+			$description => $_POST['description'],
+			$id => $_POST['id'],
+		];
+
+		var_dump($project);
 	}
 ?>
+
 
 
 <h1>Add Project</h1>
@@ -10,18 +68,23 @@
 <form method='POST'>
 	<field>
 		<label>Project Title</label>
-		<input type='text'>
+		<input name='title' value='<?=$title?>'>
+		<?php if ($titleError) { ?>
+			<p class='error'><?=$titleError?></p>
+		<?php } ?>
 	</field>
 
 	<field>
 		<label>Description</label>
-		<input type='text'>
+		<input name='description' value='<?=$description?>'>
+		<?php if ($descriptionError) { ?>
+			<p class='error'><?=$descriptionError?></p>
+		<?php } ?>
 	</field>
 
 	<field>
-		<!-- $todo: check if ID already exists before adding -->
 		<label>ID</label>
-		<input type='number'>
+		<input type='number' name='id' min='1' value='0'>
 	</field>
 
 	<button type='submit' name='add'>
