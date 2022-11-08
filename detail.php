1757f53b6092105ff491081a79b843f42fdf2374
Hjalmar Rivera <hello@hjalmar.dev>
2022-11-07 23:05:38 -0400

    Add new site with PHP routing and dynamic template

---
+++ detail.php
@@ -0,0 +1,35 @@
+<?php include('project-data.php'); ?>
+
+<?php
+	if ( isset($_GET['project']) ) {
+		$current_project_id = $_GET['project'];
+	}
+
+	// echo $current_project_id;
+
+	foreach ($project_data as $project) {
+		if ( $current_project_id == $project['id'] ) {
+			$detail = $project; 
+		}
+	}
+?>
+
+<inner-column>
+	<?php if ( isset($detail) ) { ?>
+		<h1><?=$detail['title']?></h1>
+
+		<?php include('site-menu.php'); ?>
+	
+	<picture>
+		<img src="https://peprojects.dev/images/landscape.jpg" alt="$todo">
+	</picture>
+	
+	<p>Case study<p>
+	
+	<?php } else { ?>
+		<h2>No project found.</h2>
+	
+		<p>Check the list for more projects <a href='?page=projects'>here</a></p>
+	
+	<?php } ?>
+</inner-column>
