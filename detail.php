e3f1c1bcc989257f86f4189356aae760eb2cde3c
Hjalmar Rivera <hello@hjalmar.dev>
2022-11-07 23:15:22 -0400

    Add main element for clearer HTML structure

--- detail.php
+++ detail.php
@@ -14,22 +14,24 @@
 	}
 ?>
 
-<inner-column>
-	<?php if ( isset($detail) ) { ?>
-		<h1><?=$detail['title']?></h1>
-
-		<?php include('site-menu.php'); ?>
+<main>
+	<inner-column>
+		<?php if ( isset($detail) ) { ?>
+			<h1><?=$detail['title']?></h1>
 	
-	<picture>
-		<img src="https://peprojects.dev/images/landscape.jpg" alt="$todo">
-	</picture>
-	
-	<p>Case study<p>
-	
-	<?php } else { ?>
-		<h2>No project found.</h2>
-	
-		<p>Check the list for more projects <a href='?page=projects'>here</a></p>
-	
-	<?php } ?>
-</inner-column>
+			<?php include('site-menu.php'); ?>
+		
+			<picture>
+				<img src="https://peprojects.dev/images/landscape.jpg" alt="$todo">
+			</picture>
+		
+			<p>Case study<p>
+		
+			<?php } else { ?>
+			<h2>No such project found.</h2>
+		
+			<p>Check the list for more projects <a href='?page=projects'>here!</a></p>
+		
+		<?php } ?>
+	</inner-column>
+</main>
