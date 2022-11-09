a444d3c4f4fdbf318c6aef6a90b811d610803a12
Hjalmar Rivera <hello@hjalmar.dev>
2022-11-08 23:10:47 -0400

    Add new codebase for responsive layout project

---
+++ projects/layout-garden/index.php
@@ -0,0 +1,49 @@
+<html lang='en'>
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<title>Hjalmar's Layout Garden</title>
+		<meta name='description' content='A collection of popular layouts recreated by eye and from scratch.'>
+		<meta property='og:image' content='images/blue-image.png'>
+
+		<!-- Google Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+
+		<!-- CSS Stylesheet -->
+		<link rel='stylesheet' href='css/style.css'>
+	</head>
+
+	<header>
+		<inner-column>
+			<h1>Layout Garden</h1>
+
+			<p>The <em>responsive layout</em> garden is a place to grow HTML/CSS skills by recreating "modules" from popular websites. Like a painter who studies a live model, if I can see it then I can build it.</p>
+
+			<p>References: <a href='https://www.starbucks.com/'>Starbucks</a></p>
+
+
+			<nav class='site-menu'>
+				<a href='https://peprojects.dev/alpha-5/hjalmar/'>Home</a>
+				<a href='https://peprojects.dev/alpha-5/hjalmar/?page=goals'>Goals</a>
+				<a href='https://peprojects.dev/alpha-5/hjalmar/?page=projects'>Projects</a>
+			</nav>
+		</inner-column>
+	</header>
+
+	<main class='modules'>
+		<!-- Each module can be its own section to make the CSS more specific -->
+		<section>
+			<inner-column>
+
+			</inner-column>
+		</section>
+	</main>
+
+<!-- 	<footer>
+		<inner-column>
+
+		</inner-column>
+	</footer> -->
